c1816cb19de90557e27db9f40f9e0cade05073bc
Nikita Smirnovs <124347632+Endizzy@users.noreply.github.com>
2025-09-05 15:13:44 +0300

    Add files via upload

---
+++ courier-app/constants.js
@@ -0,0 +1,7 @@
+// export const ORIGIN = 'https://deliveryappserver-1.onrender.com';
+// export const WS_URL = `${ORIGIN.replace('https', 'wss').replace('http', 'ws')}`;
+// export const API_LOCATION = `${ORIGIN}/api/location`;
+export const ORIGIN = 'https://deliveryappserver-1.onrender.com';
+export const WS_URL = ORIGIN.replace(/^http/, 'ws'); // меняет http -> ws и https -> wss
+export const API_LOCATION = `${ORIGIN}/api/location`;
+
