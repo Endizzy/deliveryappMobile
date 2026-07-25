96051493c6b0a42c5dd389236eb6e20ad85e3130
Endizzy <nikita.smirnovs_2@edu.rtu.lv>
2026-07-25 16:29:08 +0300

    origin fix

--- courier-app/constants.js
+++ courier-app/constants.js
@@ -1,6 +1,6 @@
 // export const ORIGIN = 'https://deliveryappserver-1.onrender.com';
 // export const WS_URL = `${ORIGIN.replace('https', 'wss').replace('http', 'ws')}`;
 // export const API_LOCATION = `${ORIGIN}/api/location`;
-export const ORIGIN = 'http://api.developfuture.id.lv';
+export const ORIGIN = 'https://api.developfuture.id.lv';
 export const WS_URL = ORIGIN.replace(/^http/, 'ws'); // меняет http -> ws и https -> wss
 export const API_LOCATION = `${ORIGIN}/api/location`;
